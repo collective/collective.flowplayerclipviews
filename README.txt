ff446b9d6f433a1db749e8ebdcbc274a3a98ac59
Luca Fabbri <luca@keul.it>
2012-01-30 09:05:30 +0100

    initial import

---
+++ README.txt
@@ -0,0 +1,5 @@
+Introduction
+============
+
+Add to you Plone videos (that use ``collective.flowplayer``__ or derivated products), a
+"views count" feature.
---
+++ collective/flowplayerclipviews/browser/collective.flowplayerclipviews/flowplayerclipviews.js
@@ -0,0 +1,44 @@
+/*jslint indent: 4 */
+/*global jQuery: false, jq: false, document: false, window: false, $f: false */
+
+/**
+ * JavaScript code for collective.flowplayerclipviews.
+ * Controls running time of the video and warn the server when the a video are totally seen
+ */
+
+jQuery.flowplayerclipviews = {
+	step_length: 5
+};
+
+jq(document).ready(function () {
+	"use strict";
+	var call_context = jq('base').attr('href');
+	$f("*").each(function (index) {
+		this.onStart(function (clip) {
+			if (clip.completedCuepoints !== 1) {
+				clip.completedCuepoints = 0;
+				var fullDuration = Math.ceil(clip.fullDuration);
+				var cuepoints = [];
+				var i;
+				for (i = jq.flowplayerclipviews.step_length; i < fullDuration; i += jq.flowplayerclipviews.step_length) {
+					cuepoints.push(i * 1000);
+				}
+				this.onCuepoint(cuepoints, function (clip, cuepoint) {
+					if (clip.completedCuepoints !== -1) {
+						clip.completedCuepoints += 1;
+					}
+				});
+			}
+		}).onFinish(function (clip) {
+			if (clip.completedCuepoints !== -1) {
+				var fullDuration = Math.ceil(clip.fullDuration);
+				var cuePointsToBeCompleted = parseInt(fullDuration / jq.flowplayerclipviews.step_length, 10) - 1;
+				if (cuePointsToBeCompleted === clip.completedCuepoints) {
+					alert('Done');
+				}
+				// seeing this video again and again will not trigger a new view count
+				clip.completedCuepoints = -1;
+			}
+		});
+	});
+});
